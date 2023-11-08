d0786e4e8ce4719f52ea062d9c14bf4df7359d1d
Frusti007 <Frusti007@hotmail.com>
2023-11-08 09:47:03 +0100

    Update onfigurations and remove unused files

---
+++ scripts/rsync-pull.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DEST_DIR="/home/"
+SRC_DIR="joshua@192.168.81.161:/home/joshua/BACKUP/"
+
+RSYNC_OPTS="-ahvz --delete --progress"
+
+rsync $RSYNC_OPTS $SRC_DIR $DEST_DIR 2>&1 | tee /var/log/rsync.log
